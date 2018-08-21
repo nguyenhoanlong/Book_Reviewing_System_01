b895c3e0bb793a4ee86412c556ca872efd997679
Nguyen Hoan Long <nguyen.hoan.long@framgia.com>
2018-08-21 15:57:01 +0700

    Demo

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   has_many :books
+  validates_presence_of :title
   scope :list, -> {select(:id, :title, :description).order(title: :asc)}
   scope :by_title, ->(cat_name){select(:id, :title, :description).order(title: :asc)
     .where("title like ?", "%#{cat_name}%")}
